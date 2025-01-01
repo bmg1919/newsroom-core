10bb395c5fed764421653b9653069d2578e9ea14
Bryan Garland <bmg1919@outlook.com>
2025-01-01 18:12:14 -0500

    What am I doing?

--- newsroom/commands/manager.py
+++ newsroom/commands/manager.py
@@ -1,7 +1,7 @@
 from flask_script import Manager
-
 from newsroom.web.factory import get_app
-
+from flask import Flask
 
 app = get_app()
-manager = Manager(app)
+# manager = Manager(app)
+flask = Flask(app)
