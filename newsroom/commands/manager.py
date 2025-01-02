5b43ffa4de62494ac0ec6f3f80a09b1f7eedaedc
Bryan Garland <bmg1919@outlook.com>
2025-01-01 19:18:52 -0500

    flask changes

--- newsroom/commands/manager.py
+++ newsroom/commands/manager.py
@@ -1,7 +1,8 @@
 # from flask_script import Manager
 from newsroom.web.factory import get_app
-from flask import Flask
+from flask import Flask, cli
+
 
 app = get_app()
 # manager = Manager(app)
-flask = Flask(app)
+# flask = Flask(app)
